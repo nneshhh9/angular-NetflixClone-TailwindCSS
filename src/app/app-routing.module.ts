b00d0aae7375e3637a75bafa3fe44d4e5999f489
Vlad Kozhuhov <nneshhh167@gmail.com>
2024-01-07 03:23:19 +0500

    добавил вход через гугл, ошибка с loadComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BrowseComponent } from './browse/browse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    // component: LoginComponent
+    loadComponent: ()=> 
+      import('./login/login.component').then( mod => mod.LoginComponent)
+  },
+  {
+    path: 'browse', 
+    // component: BrowseComponent
+    // loadComponent: ()=> 
+    //   import('./browse/browse.component').then( mod => mod.BrowseComponent)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
