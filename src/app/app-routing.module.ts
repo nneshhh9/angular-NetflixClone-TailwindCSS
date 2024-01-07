d9cfc0663e39fe28e1c6713a665517c509a79844
Vlad Kozhuhov <nneshhh167@gmail.com>
2024-01-07 19:59:23 +0500

    сделал аутентификацию через гугл, с loadComponent не разобрался

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { BrowseComponent } from './browse/browse.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '', 
-    // component: LoginComponent
-    loadComponent: ()=> 
-      import('./login/login.component').then( mod => mod.LoginComponent)
+    component: LoginComponent
+    // loadComponent: () => 
+    //   import('./login/login.component').then(mod => mod.LoginComponent)
   },
   {
     path: 'browse', 
-    // component: BrowseComponent
-    // loadComponent: ()=> 
-    //   import('./browse/browse.component').then( mod => mod.BrowseComponent)
+    component: BrowseComponent
+    // loadComponent: () => 
+    //   import('./browse/browse.component').then(mod => mod.BrowseComponent)
   },
+  // {
+  //   path: '',
+  //   loadComponent: () => import('').then(mod => mod. )
+  // }
 
 ];
 
--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-browse',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent {
+  auth = inject(AuthService);
+  nameUser = JSON.parse(sessionStorage.getItem("loggedInUser")!).name;
+  emailUser = JSON.parse(sessionStorage.getItem("loggedInUser")!).email;
+  profileUserImg = JSON.parse(sessionStorage.getItem("loggedInUser")!).picture;
 
+  signOut() {
+    sessionStorage.removeItem("loggedInUser");
+    this.auth.singOut();
+  }
 }
